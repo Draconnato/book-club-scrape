7eed3af8319b4914c959f90c534e0f7ee2235f24
Gabriel Palma de Oliveira <gabriel.palma.oliveira@hotmail.com>
2023-08-22 21:54:55 -0300

    end the getCategoryList function to receive a dict from website

--- Script/scraper.py
+++ Script/scraper.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup, NavigableString
+from pprint import pprint
+
 
 URL = "https://books.toscrape.com/"
 
@@ -20,22 +22,24 @@ def getSourcePage(url):
 
     return source
     
-page = getSourcePage(url=URL)
+#page = getSourcePage(url=URL)
 
-soup = BeautifulSoup(page,'html.parser')
+def getCategoryList(pageHtml):
 
+    soup = BeautifulSoup(pageHtml,'html.parser')
 
-for listClass in soup.find_all('ul',class_="nav nav-list"):
-    for list in listClass.find_all('li'):
-        
-        if isinstance(list,NavigableString):
-            continue
-        else:
-            for childrenList in list.find_all('ul'):
-                for novaChildrenList in childrenList.find_all('li'):
-                    print(novaChildrenList.a.text.strip())
+    categoryList = {}
 
+    for listClass in soup.find_all('ul',class_="nav nav-list"):
+        for list in listClass.find_all('li'):
 
+            if isinstance(list,NavigableString):
+                continue
+            else:
+                for childrenList in list.find_all('ul'):
+                    for newChildrenList in childrenList.find_all('li'):
+                        categoryList.update({newChildrenList.a.text.strip():newChildrenList.a['href'].strip()})
+    
+    driver.quit
 
-
-driver.quit
+    return categoryList
