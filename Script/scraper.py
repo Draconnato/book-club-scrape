4755ace8660e52ea30e1e2dc45d0fe16e461348b
Draconnato <Gabriel.palma.oliveira@hotmail.com>
2023-08-24 22:25:50 -0300

    Mapping the fields from books - not done yet

--- Script/scraper.py
+++ Script/scraper.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup, NavigableString
 
+import re
 import time
 
 """
@@ -34,9 +35,9 @@ page = getSourcePage(url=URL)
 
 def getCategoryList(pageHtml):
 
-    soup = BeautifulSoup(pageHtml,'html.parser')
-
     categoryList = {}
+
+    soup = BeautifulSoup(pageHtml,'html.parser')
 
     for listClass in soup.find_all('ul',class_="nav nav-list"):
         for list in listClass.find_all('li'):
@@ -54,10 +55,26 @@ def getCategoryList(pageHtml):
 
 categoryURL = getCategoryList(page)
 
+def getData(pageContent):
+
+    soup = BeautifulSoup(pageContent,'html.parser')
+
+    for i in soup.find_all('ol',class_='row'):
+        for j in i('li'):
+            
+            print(j)
+            #print(j.h3.a['title']) # Book_Title
+            #print(j.find('p',class_="price_color").string) #Book_Value
+            #print(j.find('p',class_='instock availability').text.strip()) #Book_availability
+            #print(j.find('p',class_=re.compile("^star-rating"))['class'][1]) #Book_stars
+            #print(j.find('img')['src']) #book_image (needs concatenate with site URL)
+
+getData(page)
+
+"""
 for i in categoryURL:
 
     currentCategoryURL = categoryURL[i]
 
-    driver.get(URL+currentCategoryURL)  
-
-    time.sleep(3)   
+    pageContent = getSourcePage(URL+currentCategoryURL)  
+    """
