c532f4cd8af7f419f68eeb395174589558e1f7ad
Gabriel Palma de Oliveira <gabriel.palma.oliveira@hotmail.com>
2023-09-07 19:24:31 -0300

    new method to scraper: run the entire job, add files to gitignore and start with parquet files in main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import script.scraper as scraper
+import pyarrow.parquet as pq
+import pyarrow as pa
 
 URL = "https://books.toscrape.com/"
 
-chrome = scraper.launchChrome(visibility=1) #start selenium
+chrome = scraper.launchChrome() #start selenium
 
 page = scraper.getSourcePage(chrome,URL) #get a page content from a source page - required
 
-#categoryURL = scraper.getCategoryList(page) # get a category list and URL - optional
+categoryURL = scraper.getCategoryList(page) # get a category list and URL - required
 
-#BookData = scraper.getBookData(page) # receive a json with data from all books in a page - optional
-
-
-"""
 for i in categoryURL:
+    currentCategory = categoryURL[i]
+    book = scraper.scrapePage(chrome,URL+currentCategory)
 
-    currentCategoryURL = categoryURL[i]
+    book_test = pa.Table.from_pylist(book)
 
-    pageContent = getSourcePage(URL+currentCategoryURL)  
-    """
+    pq.write_table(book_test,'files/'+i+'.parquet')
+
+# need to fix the filenames and works with dataset (partition files: https://arrow.apache.org/docs/python/parquet.html#reading-from-partitioned-datasets)
